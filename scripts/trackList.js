0148109fe4f7b68a323dcb8ffbf0cf7f72bd51e4
Keenan <Keenan@Keenans-MacBook-Pro.local>
2016-06-21 19:36:03 -0700

    all spotify ajax calls and gettemplate calls are now working in trackList.js

--- scripts/trackList.js
+++ scripts/trackList.js
@@ -3,6 +3,7 @@ var trackList = {};
 // trackList.All = [];
 
 trackList.toHtml = function(track){
+  $('#track-list').empty();
   getCompiledTemplate("track").then(function(handlebarsCompile){
     console.log(handlebarsCompile);
     var html = handlebarsCompile(track);
@@ -29,13 +30,53 @@ trackList.genreFetch = function(){
         trackList.toHtml(track);
         console.log(track);
       })
-      // console.log(trackList.All);
     })
   });
 };
 
+trackList.yearFetch = function(){
+  $('#year-form').submit(function(e){
+    e.preventDefault();
+    var selectYear = $(this).find('#year-select , option:selected').val();
+    console.log(selectYear);
+    $.ajax({
+      type: 'POST',
+      url: '/yearTracks',
+      data: {
+        'year' : selectYear
+      }
+    })
+    .then(function(data){
+      data.forEach(function(track){
+        trackList.toHtml(track);
+      })
+    })
+  });
+}
+trackList.termFetch = function(){
+  $('#term-form').submit(function(e){
+    e.preventDefault();
+    var inputTerm = $(this).find('input').val();
+    console.log(inputTerm);
+    $.ajax({
+      type: 'POST',
+      url: '/termTracks',
+      data: {
+        'term' : inputTerm
+      }
+    })
+    .then(function(data){
+      data.forEach(function(track){
+        trackList.toHtml(track);
+      })
+    })
+  });
+}
+
 trackList.initAll = function(){
   trackList.genreFetch();
+  trackList.yearFetch();
+  trackList.termFetch();
 }
 
 module.trackList = trackList;
